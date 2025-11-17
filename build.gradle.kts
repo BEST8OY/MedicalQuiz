10b7548fcceedccea73dcd501c84e77d1a160e54
best8oy <alireza.bestboy@outlook.com>
2025-11-17 04:17:02 +0330

    frad

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.2.0" apply false
+    // Android Gradle Plugin updated to support compileSdk 35 for modern Compose libs
+    id("com.android.application") version "8.6.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
 }
 
