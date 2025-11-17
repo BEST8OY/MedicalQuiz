057a40f53145ebdfe2519f15b80ede889d724c70
best8oy <alireza.bestboy@outlook.com>
2025-11-17 19:12:33 +0330

    vvx

--- app/src/main/java/com/medicalquiz/app/ui/QuizTopBar.kt
+++ app/src/main/java/com/medicalquiz/app/ui/QuizTopBar.kt
@@ -5,6 +5,7 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.ui.Modifier
 import androidx.compose.runtime.Composable
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material.icons.Icons
