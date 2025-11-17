01afff5d5cb8bee15e45133c73e163afb2ef6051
best8oy <alireza.bestboy@outlook.com>
2025-11-18 03:09:03 +0330

    ta

--- app/src/main/java/com/medicalquiz/app/ui/QuizRoot.kt
+++ app/src/main/java/com/medicalquiz/app/ui/QuizRoot.kt
@@ -31,6 +31,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.ui.unit.dp
