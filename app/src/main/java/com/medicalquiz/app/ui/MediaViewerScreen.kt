d1e71083afd395a42ea0500f4f74d51959b260d6
best8oy <alireza.bestboy@outlook.com>
2025-11-18 04:01:03 +0330

    fdg

--- app/src/main/java/com/medicalquiz/app/ui/MediaViewerScreen.kt
+++ app/src/main/java/com/medicalquiz/app/ui/MediaViewerScreen.kt
@@ -6,6 +6,7 @@ import android.net.Uri
 import android.os.Environment
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.material3.Button
 import androidx.compose.foundation.layout.Column
