1d064588d8128353661bb3a5ebb889ddec879f1d
best8oy <alireza.bestboy@outlook.com>
2026-02-09 14:25:47 +0330

    import

--- composeApp/src/commonMain/kotlin/com/medicalquiz/app/shared/ui/screens/quiz/QuizScreen.kt
+++ composeApp/src/commonMain/kotlin/com/medicalquiz/app/shared/ui/screens/quiz/QuizScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.animation.core.spring
 import androidx.compose.animation.expandVertically
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
