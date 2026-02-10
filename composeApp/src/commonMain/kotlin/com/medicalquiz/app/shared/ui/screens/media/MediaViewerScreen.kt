fc91cc304ab2669daef0b9f41520cf408b3296a2
best8oy <alireza.bestboy@outlook.com>
2026-02-11 03:00:25 +0330

    build

--- composeApp/src/commonMain/kotlin/com/medicalquiz/app/shared/ui/screens/media/MediaViewerScreen.kt
+++ composeApp/src/commonMain/kotlin/com/medicalquiz/app/shared/ui/screens/media/MediaViewerScreen.kt
@@ -58,6 +58,7 @@ import androidx.compose.material3.ButtonGroupDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.FilledIconButton
+import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
