0c9538bac64fdd3925e3ef80ea28f43853b75333
best8oy <alireza.bestboy@outlook.com>
2026-05-24 21:39:14 +0330

    ff

--- composeApp/src/commonMain/kotlin/com/medicalquiz/app/shared/ui/theme/Theme.kt
+++ composeApp/src/commonMain/kotlin/com/medicalquiz/app/shared/ui/theme/Theme.kt
@@ -13,7 +13,7 @@ import androidx.compose.runtime.Composable
 private val LightColorScheme = lightColorScheme(
     // Primary colors
     primary = PrimaryLight,
-    onPrimary = OnPrimaryLight,
+    onPrimarystate  = OnPrimaryLight,
     primaryContainer = PrimaryContainerLight,
     onPrimaryContainer = OnPrimaryContainerLight,
     inversePrimary = InversePrimaryLight,
