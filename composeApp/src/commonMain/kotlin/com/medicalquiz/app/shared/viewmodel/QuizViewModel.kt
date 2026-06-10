26a5eec6cc9347cebac0b1761dfca7e3586cec3f
best8oy <alireza.bestboy@outlook.com>
2026-06-10 03:43:27 +0330

    dammit

--- composeApp/src/commonMain/kotlin/com/medicalquiz/app/shared/viewmodel/QuizViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/medicalquiz/app/shared/viewmodel/QuizViewModel.kt
@@ -16,6 +16,7 @@ import com.medicalquiz.app.shared.ui.state.QuizUiState
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.withContext
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
