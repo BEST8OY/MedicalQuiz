50e64dd686ea918f935e06ee1c5114066818228b
best8oy <alireza.bestboy@outlook.com>
2026-06-19 14:52:21 +0330

    mhl

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -2,9 +2,17 @@ package com.medicalquiz.app.shared
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import org.jetbrains.compose.resources.painterResource
+import medicalquiz.composeapp.generated.resources.Res
+import medicalquiz.composeapp.generated.resources.app_icon
 
 fun main() = application {
-    Window(onCloseRequest = ::exitApplication, title = "MedicalQuiz") {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "MedicalQuiz",
+        icon = painterResource(Res.drawable.app_icon)
+    ) {
         App()
     }
 }
+
