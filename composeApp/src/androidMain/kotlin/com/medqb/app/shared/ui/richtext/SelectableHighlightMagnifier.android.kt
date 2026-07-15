e04a4c439f90b2648ec55577d8b0330d0b114fa5
best8oy <alireza.bestboy@outlook.com>
2026-07-15 21:46:42 +0330

    feat: make magnifier rounded (CircleShape) for Material 3 look

--- composeApp/src/androidMain/kotlin/com/medqb/app/shared/ui/richtext/SelectableHighlightMagnifier.android.kt
+++ composeApp/src/androidMain/kotlin/com/medqb/app/shared/ui/richtext/SelectableHighlightMagnifier.android.kt
@@ -2,6 +2,7 @@ package com.medqb.app.shared.ui.richtext
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.magnifier
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.unit.Density
@@ -13,4 +14,5 @@ actual fun Modifier.platformSelectionMagnifier(
 ): Modifier = this.magnifier(
     sourceCenter = sourceCenter,
     magnifierCenter = magnifierCenter,
+    shape = CircleShape,
 )
