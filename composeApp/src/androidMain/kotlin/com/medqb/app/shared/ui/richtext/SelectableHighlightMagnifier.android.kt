61aab98d3fd349170f2af7b32e5be7fbd9422d9b
best8oy <alireza.bestboy@outlook.com>
2026-07-15 22:02:27 +0330

    fix: remove shape parameter (not available in this Compose version)

--- composeApp/src/androidMain/kotlin/com/medqb/app/shared/ui/richtext/SelectableHighlightMagnifier.android.kt
+++ composeApp/src/androidMain/kotlin/com/medqb/app/shared/ui/richtext/SelectableHighlightMagnifier.android.kt
@@ -2,7 +2,6 @@ package com.medqb.app.shared.ui.richtext
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.magnifier
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.unit.Density
@@ -14,5 +13,4 @@ actual fun Modifier.platformSelectionMagnifier(
 ): Modifier = this.magnifier(
     sourceCenter = sourceCenter,
     magnifierCenter = magnifierCenter,
-    shape = CircleShape,
 )
